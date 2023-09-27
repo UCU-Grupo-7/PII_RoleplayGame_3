93f538ca1daa40cfbd14acec6584ae986045374b
nanosanchez22 <nanosanchez22@gmail.com>
2023-09-27 09:47:42 -0300

    tests

--- src/test/Test.Library/UnitTest1.cs
+++ src/test/Test.Library/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
-namespace Test.Library
+namespace RoleplayGame
 {
     public class Tests
     {
@@ -10,9 +11,41 @@ namespace Test.Library
         }
 
         [Test]
-        public void Test1()
+        public void Curar()
+        {
+            Dwarf gimli = new Dwarf("Gimli");
+            gimli.Cure();
+            Assert.That(gimli.Health, Is.EqualTo(100));
+        }
+        [Test]
+        public void Atacar()
+        {
+            SpellsBook book = new SpellsBook();
+            book.Spells = new Spell[]{ new Spell() };
+
+            Wizard gandalf = new Wizard("Gandalf");
+            gandalf.Staff = new Staff();
+            gandalf.SpellsBook = book; 
+
+            Dwarf gimli = new Dwarf("Gimli");
+            gimli.DefenseItem = new List<IDefenseItem>() {new Helmet(), new Shield()} ;
+            gimli.AttackItem = new Axe();
+            
+
+            int gimliStartHealth = gimli.Health;  // vida con la que arranca gimli
+            
+            gandalf.Attack(gimli);
+
+            //Assert.That(gimli.Health, Is.EqualTo(40));
+            Assert.That(gimli.Health, Is.EqualTo(gimliStartHealth -= gandalf.AttackValue - gimli.DefenseValue));
+        }
+        [Test]
+        public void Test3()
         {
             Assert.Pass();
         }
+        
     }
+
+    
 }
